c250cf4918c4f67465ed6e07a4a9afc242e2f2d0
Kusuma <sensnerd@gmail.com>
2023-05-27 12:04:02 +0800

    cancel the use retrieve method to get product

--- apps/products/views.py
+++ apps/products/views.py
@@ -2,9 +2,7 @@ from rest_framework import viewsets
 from django.contrib.auth import get_user_model
 from rest_framework import filters
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
-from rest_framework import status
 
 from .models import Category, Product, Subcategory, Tag, Brand, Rating, Stock
 from .serializers import (
@@ -23,11 +21,18 @@ User = get_user_model()
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    # permission_classes = [IsAdminOrReadOnly]
-    permission_classes = [AllowAny]
+    permission_classes = [IsAdminOrReadOnly]
     filter_backends = [filters.SearchFilter, filters.OrderingFilter, DjangoFilterBackend]
     search_fields = ['name', 'description']
-    filterset_fields = ['category__slug', 'subcategory__slug', 'brand__slug']
+    filterset_fields = {
+        'category': ['exact'],
+        'subcategory': ['exact'],
+        'tags': ['exact'],
+        'brand': ['exact'],
+        'price': ['exact', 'gte', 'lte'],
+        'created_at': ['exact', 'gte', 'lte'],
+        'product_ratings__star': ['exact', 'gte', 'lte'],
+    }
     ordering_fields = ['name', 'price', 'created_at', 'product_ratings__star']
     ordering = ['-created_at']
     lookup_field = 'slug'
@@ -36,47 +41,25 @@ class ProductViewSet(viewsets.ModelViewSet):
         instance = self.get_object()
         serializer = self.get_serializer(instance)
 
-        # get product ratings
-        ratings = Rating.objects.filter(product=instance)
-
-        # enable filters.OrderingFilter on ratings
-        ordering = request.query_params.get('ordering', None)
-        if ordering:
-            if ordering.startswith('-'):
-                field = ordering[1:]
-                reverse = True
-            else:
-                field = ordering
-                reverse = False
-
-            # apply ordering to ratings queryset
-            if field in ['star', 'created_at']:
-                ratings = ratings.order_by(ordering)
-            else:
-                ratings = ratings.order_by('-star')
-        else:
-            ratings = ratings.order_by('-star')
-
-        # add pagination to ratings
-        paginator = PageNumberPagination()
-        paginator.page_size = 5
-        paginated_ratings = paginator.paginate_queryset(ratings, request)
-        ratings_serializer = RatingSerializer(paginated_ratings, many=True)
-        
         # get product stock
         stock = Stock.objects.filter(product=instance)
         stock_serializer = StockSerializer(stock, many=True)
 
+        # get 5 latest ratings with 5 stars
+        ratings = Rating.objects.filter(product=instance, star=5).order_by('-created_at')[:5]
+        ratings_serializer = RatingSerializer(ratings, many=True)
+
+        # acumulate all ratings
+        ratings = Rating.objects.filter(product=instance)
+        total_ratings = ratings.count()
+        total_stars = sum([rating.star for rating in ratings])
+        average_rating = total_stars / total_ratings
+
         return Response({
             'product': serializer.data,
-            'ratings': ratings_serializer.data,
             'stock': stock_serializer.data,
-            'ratings_pagination': {
-                'next': paginator.get_next_link(),
-                'previous': paginator.get_previous_link(),
-                'count': paginator.page.paginator.count,
-                'num_pages': paginator.page.paginator.num_pages,
-            }
+            'total_rating': average_rating,
+            'latest_rating': ratings_serializer.data
         })
 
 class CategoryViewSet(viewsets.ModelViewSet):
@@ -88,48 +71,6 @@ class CategoryViewSet(viewsets.ModelViewSet):
     ordering_fields = ['name']
     ordering = ['name']
     lookup_field = 'slug'
-
-    def retrieve(self, request, *args, **kwargs):
-        instance = self.get_object()
-        serializer = self.get_serializer(instance)
-
-        # get category products
-        products = Product.objects.filter(category=instance)
-
-        # enable filters.OrderingFilter on products
-        ordering = request.query_params.get('ordering', None)
-        if ordering:
-            if ordering.startswith('-'):
-                field = ordering[1:]
-                reverse = True
-            else:
-                field = ordering
-                reverse = False
-
-            # apply ordering to products queryset
-            if field in ['name', 'price', 'created_at', 'product_ratings__star']:
-                products = products.order_by(ordering)
-            else:
-                products = products.order_by('-created_at')
-        else:
-            products = products.order_by('-created_at')
-
-        # add pagination to products
-        paginator = PageNumberPagination()
-        paginator.page_size = 10
-        paginated_products = paginator.paginate_queryset(products, request)
-        products_serializer = ProductSerializer(paginated_products, many=True)
-
-        return Response({
-            'category': serializer.data,
-            'products': products_serializer.data,
-            'products_pagination': {
-                'next': paginator.get_next_link(),
-                'previous': paginator.get_previous_link(),
-                'count': paginator.page.paginator.count,
-                'num_pages': paginator.page.paginator.num_pages,
-            }
-        })
     
 class SubcategoryViewSet(viewsets.ModelViewSet):
     queryset = Subcategory.objects.all()
@@ -140,48 +81,6 @@ class SubcategoryViewSet(viewsets.ModelViewSet):
     ordering_fields = ['name']
     ordering = ['name']
     lookup_field = 'slug'
-
-    def retrieve(self, request, *args, **kwargs):
-        instance = self.get_object()
-        serializer = self.get_serializer(instance)
-
-        # get subcategory products
-        products = Product.objects.filter(subcategory=instance)
-
-        # enable filters.OrderingFilter on products
-        ordering = request.query_params.get('ordering', None)
-        if ordering:
-            if ordering.startswith('-'):
-                field = ordering[1:]
-                reverse = True
-            else:
-                field = ordering
-                reverse = False
-
-            # apply ordering to products queryset
-            if field in ['name', 'price', 'created_at', 'product_ratings__star']:
-                products = products.order_by(ordering)
-            else:
-                products = products.order_by('-created_at')
-        else:
-            products = products.order_by('-created_at')
-
-        # add pagination to products
-        paginator = PageNumberPagination()
-        paginator.page_size = 10
-        paginated_products = paginator.paginate_queryset(products, request)
-        products_serializer = ProductSerializer(paginated_products, many=True)
-
-        return Response({
-            'subcategory': serializer.data,
-            'products': products_serializer.data,
-            'products_pagination': {
-                'next': paginator.get_next_link(),
-                'previous': paginator.get_previous_link(),
-                'count': paginator.page.paginator.count,
-                'num_pages': paginator.page.paginator.num_pages,
-            }
-        })
 
 class TagViewSet(viewsets.ModelViewSet):
     queryset = Tag.objects.all()
@@ -202,55 +101,16 @@ class BrandViewSet(viewsets.ModelViewSet):
     ordering_fields = ['name']
     ordering = ['name']
     lookup_field = 'slug'
-
-    def retrieve(self, request, *args, **kwargs):
-        instance = self.get_object()
-        serializer = self.get_serializer(instance)
-
-        # get brand products
-        products = Product.objects.filter(brand=instance)
-
-        # enable filters.OrderingFilter on products
-        ordering = request.query_params.get('ordering', None)
-        if ordering:
-            if ordering.startswith('-'):
-                field = ordering[1:]
-                reverse = True
-            else:
-                field = ordering
-                reverse = False
-
-            # apply ordering to products queryset
-            if field in ['name', 'price', 'created_at', 'product_ratings__star']:
-                products = products.order_by(ordering)
-            else:
-                products = products.order_by('-created_at')
-        else:
-            products = products.order_by('-created_at')
-
-        # add pagination to products
-        paginator = PageNumberPagination()
-        paginator.page_size = 10
-        paginated_products = paginator.paginate_queryset(products, request)
-        products_serializer = ProductSerializer(paginated_products, many=True)
-
-        return Response({
-            'brand': serializer.data,
-            'products': products_serializer.data,
-            'products_pagination': {
-                'next': paginator.get_next_link(),
-                'previous': paginator.get_previous_link(),
-                'count': paginator.page.paginator.count,
-                'num_pages': paginator.page.paginator.num_pages,
-            }
-        })
     
 class RatingViewSet(viewsets.ModelViewSet):
     queryset = Rating.objects.all()
     serializer_class = RatingSerializer
     permission_classes = [AllowAny]
     filter_backends = [filters.OrderingFilter, DjangoFilterBackend]
-    filterset_fields = ['star']
+    filterset_fields = {
+        'product': ['exact'],
+        'star': ['exact', 'gte', 'lte'],
+    }
     ordering_fields = ['star', 'created_at']
     ordering = ['-created_at']
     
@@ -260,6 +120,10 @@ class StockViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAdminOrReadOnly]
     filter_backends = [filters.SearchFilter, filters.OrderingFilter, DjangoFilterBackend]
     search_fields = ['product__name']
-    filterset_fields = ['purchase_date', 'expiry_date']
+    filterset_fields = {
+        'product': ['exact'],
+        'purchase_date': ['exact', 'gte', 'lte'],
+        'expiry_date': ['exact', 'gte', 'lte'],
+    }
     ordering_fields = ['product__name', 'purchase_date', 'expiry_date']
     ordering = ['-purchase_date']
