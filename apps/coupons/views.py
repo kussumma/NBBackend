0865622abca752ecae584bf60db1bc9ae742fa0e
Kusuma <sensnerd@gmail.com>
2023-06-01 13:56:45 +0800

    update coupons view

--- apps/coupons/views.py
+++ apps/coupons/views.py
@@ -1,5 +1,10 @@
 from rest_framework import viewsets
-from rest_framework import permissions
+from rest_framework import filters
+from django_filters.rest_framework import DjangoFilterBackend
+
+from apps.products.permissions import (
+    IsAdminOrReadOnly,
+)
 
 from .models import (
     DiscountType,
@@ -16,15 +21,33 @@ from .serializers import (
 class DiscountTypeViewSet(viewsets.ModelViewSet):
     queryset = DiscountType.objects.all()
     serializer_class = DiscountTypeSerializer
-    permission_classes = [permissions.AllowAny]
+    permission_classes = [IsAdminOrReadOnly]
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['name']
+    ordering_fields = ['name']
+    ordering = ['name']
 
 class CouponViewSet(viewsets.ModelViewSet):
     queryset = Coupon.objects.all()
     serializer_class = CouponSerializer
-    permission_classes = [permissions.AllowAny]
+    permission_classes = [IsAdminOrReadOnly]
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter, DjangoFilterBackend]
+    search_fields = ['code', 'name']
+    ordering_fields = ['name', 'code', 'valid_from', 'valid_to']
+    filterset_fields = {
+        'discount_type': ['exact'],
+        'code': ['exact'],
+        'is_active': ['exact'],
+        'valid_from': ['exact', 'gte', 'lte'],
+        'valid_to': ['exact', 'gte', 'lte'],
+    }
+    ordering = ['name', 'code', 'valid_from', 'valid_to']
 
 class CouponUserViewSet(viewsets.ModelViewSet):
     queryset = CouponUser.objects.all()
     serializer_class = CouponUserSerializer
-    permission_classes = [permissions.AllowAny]
+    permission_classes = [IsAdminOrReadOnly]
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['coupon__code', 'user__email']
+    ordering_fields = ['coupon__code', 'user__email']
 
