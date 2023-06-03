5d8691c419b69a2d53cf938548a480b852709c28
Kusuma <sensnerd@gmail.com>
2023-06-04 01:06:36 +0800

    add listview user & staff

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -1,7 +1,9 @@
 from django.urls import include, path
 
-from .views import UserDetailsView
+from .views import UserDetailsView, UserListView, StaffListView
 
 urlpatterns = [
     path('account/<uuid:id>/', UserDetailsView.as_view(), name='user-details'),
+    path('account/list/', UserListView.as_view(), name='user-list'),
+    path('account/staff/', StaffListView.as_view(), name='staff-list'),
 ]
--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,6 +1,7 @@
-from rest_framework.generics import RetrieveUpdateAPIView
+from rest_framework.generics import RetrieveUpdateAPIView, ListAPIView
 from django.contrib.auth import get_user_model
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
+from rest_framework import filters
 
 from .serializers import UserSerializer
 
@@ -13,5 +14,27 @@ class UserDetailsView(RetrieveUpdateAPIView):
 
     def get_object(self):
         return self.request.user
+    
+class UserListView(ListAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+    permission_classes = [IsAdminUser]
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['email', 'first_name', 'last_name']
+    ordering_fields = ['email', 'first_name', 'last_name']
+    ordering = ['-date_joined']
 
+    def get_queryset(self):
+        return User.objects.filter(is_staff=False)
+    
+class StaffListView(ListAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+    permission_classes = [IsAdminUser]
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['email', 'first_name', 'last_name']
+    ordering_fields = ['email', 'first_name', 'last_name']
+    ordering = ['-date_joined']
 
+    def get_queryset(self):
+        return User.objects.filter(is_staff=True)
