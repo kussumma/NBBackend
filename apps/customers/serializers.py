02892c63361b85f0435d2fabf9fc4d9a7e3235ab
Kusuma <sensnerd@gmail.com>
2023-06-09 12:22:49 +0800

    remove editable and update serializer

--- apps/customers/serializers.py
+++ apps/customers/serializers.py
@@ -9,11 +9,13 @@ class FavoriteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Favorite
         fields = '__all__'
+        read_only_fields = ['user']
 
 class ComplaintSerializer(serializers.ModelSerializer):
     class Meta:
         model = Complaint
         fields = '__all__'
+        read_only_fields = ['user']
 
 class ComplaintImageSerializer(serializers.ModelSerializer):
     class Meta:
@@ -24,16 +26,19 @@ class ProductRequestSerializer(serializers.ModelSerializer):
     class Meta:
         model = ProductRequest
         fields = '__all__'
+        read_only_fields = ['user']
 
 class FeatureRequestSerializer(serializers.ModelSerializer):
     class Meta:
         model = FeatureRequest
         fields = '__all__'
+        read_only_fields = ['user']
 
 class BugReportSerializer(serializers.ModelSerializer):
     class Meta:
         model = BugReport
         fields = '__all__'
+        read_only_fields = ['user']
 
 class BugReportImageSerializer(serializers.ModelSerializer):
     class Meta:
