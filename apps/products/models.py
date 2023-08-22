5db439c942b66b8f4d1113392167f25352574158
Kusuma J. <sensnerd@gmail.com>
2023-08-22 21:54:58 +0800

    add product weight & dimension

--- apps/products/models.py
+++ apps/products/models.py
@@ -119,6 +119,12 @@ class Stock(models.Model):
     color = models.CharField(max_length=100, null=True, blank=True)
     other = models.CharField(max_length=100, null=True, blank=True)
     quantity = models.IntegerField(default=0)
+    weight = models.IntegerField(default=0)
+    length = models.IntegerField(default=0)
+    width = models.IntegerField(default=0)
+    height = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"{self.product.slug} - {self.price}"
