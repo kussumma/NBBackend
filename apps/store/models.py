9187ef29571c91205cf2763482a4d373b2247b72
Kusuma J. <sensnerd@gmail.com>
2023-09-02 11:34:46 +0800

    update store model

--- apps/store/models.py
+++ apps/store/models.py
@@ -25,8 +25,10 @@ class Contact(models.Model):
     longitude = models.FloatField()
     address = models.CharField(max_length=255)
     origin = models.CharField(max_length=255)
+    commodity = models.CharField(max_length=255, default='COS 2')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
