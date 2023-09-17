d5c9cae8b9ebd4fadd094ae3b8d38c4d4f9827f1
Kusuma J <sensnerd@gmail.com>
2023-09-17 20:02:41 +0800

    feat:create shipping group

--- apps/shipping/admin.py
+++ apps/shipping/admin.py
@@ -2,7 +2,14 @@ from django.contrib import admin
 from import_export import resources
 from import_export.admin import ImportExportModelAdmin
 
-from .models import ShippingRoute, Shipping
+from .models import (
+    ShippingRoute,
+    Shipping,
+    ShippingGroup,
+    ShippingGroupItem,
+    ShippingGroupTariff,
+    ShippingType
+)
 
 class ShippingRouteResource(resources.ModelResource):
     class Meta:
@@ -15,3 +22,7 @@ class ShippingRouteAdmin(ImportExportModelAdmin):
 
 admin.site.register(ShippingRoute, ShippingRouteAdmin)
 admin.site.register(Shipping)
+admin.site.register(ShippingGroup)
+admin.site.register(ShippingGroupItem)
+admin.site.register(ShippingGroupTariff)
+admin.site.register(ShippingType)
--- apps/shipping/models.py
+++ apps/shipping/models.py
@@ -15,6 +15,40 @@ class ShippingRoute(models.Model):
 
     def __str__(self):
         return f"{self.city} - {self.route}"
+    
+class ShippingGroup(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=100, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+class ShippingType(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=100, blank=True, null=True)
+    code = models.CharField(max_length=5, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+class ShippingGroupItem(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    shipping_group = models.ForeignKey(ShippingGroup, on_delete=models.CASCADE, related_name='shipping_group_items')
+    shipping_route = models.ForeignKey(ShippingRoute, on_delete=models.CASCADE, related_name='shipping_route_items')
+
+    def __str__(self):
+        return f"{self.shipping_group.name} - {self.shipping_route.name}"
+
+class ShippingGroupTariff(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    shipping_group = models.ForeignKey(ShippingGroup, on_delete=models.CASCADE, related_name='shipping_group_tariffs')
+    shipping_type = models.ForeignKey(ShippingType, on_delete=models.CASCADE, related_name='shipping_type_tariffs')
+    tariff  = models.PositiveBigIntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.shipping_group.name} - {self.shipping_type.name}"
 
 class Shipping(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
@@ -25,6 +59,7 @@ class Shipping(models.Model):
     destination = models.ForeignKey(ShippingRoute, on_delete=models.CASCADE, related_name='destination_shippings')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    is_default = models.BooleanField(default=False)
 
     def __str__(self):
         return f"{self.user} - {self.destination.route}"
