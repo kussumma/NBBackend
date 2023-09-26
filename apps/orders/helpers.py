3e2fa32b3574c8075294dc55ea03a0b9404a875d
Kusuma J <sensnerd@gmail.com>
2023-09-26 17:04:01 +0800

    feat: create email confirmation

--- apps/orders/helpers.py
+++ apps/orders/helpers.py
@@ -1,5 +1,10 @@
 from rest_framework import serializers, status
 from rest_framework.response import Response
+import datetime
+from django.conf import settings
+from django.template.loader import render_to_string
+from django.utils.html import strip_tags
+from django.core.mail import EmailMultiAlternatives
 
 from django.conf import settings
 
@@ -66,5 +71,50 @@ def lionparcel_booking(order_id):
             raise serializers.ValidationError(booking['message']['en'])
     except Exception as e:
         raise serializers.ValidationError(str(e))
+    
+    return
+
+def send_order_confirmation_email(order_id):
+    order = Order.objects.get(id=order_id)
+    if not order:
+        return Response({'error': 'Order not found'}, status=status.HTTP_400_BAD_REQUEST)
+    
+    # send email to user
+    try:
+        # Render the HTML template with the order data
+        data = {
+            'order_ref_code': order.ref_code,
+            'shipping_ref_code': order.order_shipping.shipping_ref_code,
+            'order_date': order.created_at.strftime('%d %B %Y'),
+            'customer_name': f'{order.user.first_name} {order.user.last_name}',
+            'order_items': order.order_items.all(),
+            'order_total': order.total_amount,
+            'shipping_amount': order.shipping_amount,
+            'subtotal_amount': order.subtotal_amount,
+            'tax_amount': order.tax_amount,
+            'payment': order.payment_status,
+            'year': datetime.datetime.now().year,
+        }
+
+        html_content = render_to_string('order/email/order-confirmation.html', {'order': data})
+
+        # Create a plain text version of the message for email clients that don't support HTML
+        text_content = strip_tags(html_content)
+
+        # Create the email message object
+        msg = EmailMultiAlternatives(
+            'NanoBeepa - Order Confirmation',
+            text_content,
+            settings.DEFAULT_FROM_EMAIL,
+            [order.user.email],
+        )
+
+        # Attach the HTML content to the message
+        msg.attach_alternative(html_content, "text/html")
+
+        # Send the email
+        msg.send(fail_silently=False)
+    except Exception as e:
+        return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
     
     return
