9a6f282c9859aadf15f4fb54f1f096e081bccae5
Kusuma J <sensnerd@gmail.com>
2023-10-08 23:29:53 +0800

    feat: add search for admin

--- apps/coupons/admin.py
+++ apps/coupons/admin.py
@@ -2,14 +2,44 @@ from django.contrib import admin
 
 from .models import DiscountType, Coupon, CouponUser, CouponBanner
 
-admin.site.register(DiscountType)
-admin.site.register(CouponUser)
-admin.site.register(CouponBanner)
+
+class CouponBannerInline(admin.TabularInline):
+    model = CouponBanner
+    extra = 0
+
+
+class CouponUserInline(admin.TabularInline):
+    model = CouponUser
+    extra = 0
 
 
 class CouponAdmin(admin.ModelAdmin):
     list_filter = ("is_private", "is_limited", "valid_from", "valid_to")
-    search_fields = ("prefix_code", "name")
+    search_fields = ("prefix_code", "name", "discount_type__name", "discount_value")
+    inlines = [CouponBannerInline, CouponUserInline]
+
+    def get_list_display(self, request):
+        if request.user.is_superuser:
+            return (
+                "name",
+                "prefix_code",
+                "private_decoded",
+                "discount_type",
+                "discount_value",
+            )
+        else:
+            return (
+                "name",
+                "prefix_code",
+                "discount_type",
+                "discount_value",
+            )
+
+    def private_decoded(self, obj):
+        return obj.decode_coupon_code(obj.code)
 
 
 admin.site.register(Coupon, CouponAdmin)
+admin.site.register(DiscountType)
+admin.site.register(CouponUser)
+admin.site.register(CouponBanner)
