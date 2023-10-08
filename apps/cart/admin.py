210ce14fd23f92265dc8fb1ac252665e100dab0b
Kusuma J <sensnerd@gmail.com>
2023-10-08 23:27:11 +0800

    feat: add search for cart in admin

--- apps/cart/admin.py
+++ apps/cart/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
-
 from .models import Cart, CartItem
 
-admin.site.register(Cart)
+
+class CartItemInline(admin.TabularInline):
+    model = CartItem
+    extra = 0
+
+
+class CartAdmin(admin.ModelAdmin):
+    inlines = [CartItemInline]
+    list_display = ("id", "user", "created_at", "updated_at")
+    search_fields = ("user__email", "user__first_name", "user__last_name")
+
+
+admin.site.register(Cart, CartAdmin)
 admin.site.register(CartItem)
