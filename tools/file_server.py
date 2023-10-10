e408130be8dccaffe511c33d0f6241dcfb43ec2c
Kusuma J <sensnerd@gmail.com>
2023-10-10 15:46:23 +0800

    fix: render the file directly in browser

--- tools/file_server.py
+++ tools/file_server.py
@@ -1,11 +1,9 @@
-from rest_framework import views
+from django.http import HttpResponse
+from rest_framework import status, views
 from rest_framework.response import Response
-from rest_framework import status
 from pymongo import MongoClient
 from gridfs import GridFS
 from django.conf import settings
-from django.http import HttpResponse
-import urllib.parse
 from rest_framework.permissions import AllowAny
 
 
@@ -17,27 +15,15 @@ class FileAPIView(views.APIView):
             return Response(
                 {"error": "filename is required"}, status=status.HTTP_400_BAD_REQUEST
             )
+        try:
+            client = MongoClient(settings.MONGODB_GRIDFS["URL"])
+            db = client[settings.MONGODB_GRIDFS["DB"]]
+            fs = GridFS(db, collection)
+            file = fs.get_last_version(filename)
 
-        if settings.MONGODB_GRIDFS["UNIX_SOCKET_PATH"]:
-            mongo_uri = "mongodb://" + urllib.parse.quote(
-                settings.MONGODB_GRIDFS["UNIX_SOCKET_PATH"]
-            )
-            client = MongoClient(mongo_uri)
-        else:
-            client = MongoClient(
-                settings.MONGODB_GRIDFS["HOST"],
-                settings.MONGODB_GRIDFS["PORT"],
-            )
-        db = client[settings.MONGODB_GRIDFS["DB"]]
-        if settings.MONGODB_GRIDFS["USERNAME"] and settings.MONGODB_GRIDFS["PASSWORD"]:
-            db.authenticate(
-                settings.MONGO_DB_USERNAME,
-                settings.MONGO_DB_PASSWORD,
-            )
+            response = HttpResponse(file.read(), content_type="image/jpeg")
+            response["Content-Disposition"] = "inline; filename=" + file.filename
+        except Exception as e:
+            return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
 
-        fs = GridFS(db, collection)
-        file = fs.get_last_version(filename)
-
-        response = HttpResponse(file.read(), content_type="image/jpeg")
-        response["Content-Disposition"] = "attachment; filename=" + file.filename
         return response
