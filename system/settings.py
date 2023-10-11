e929904ef8d52028fe19fa675cefb0019cb9277a
Kusuma J <sensnerd@gmail.com>
2023-10-11 16:21:20 +0800

    feat: throttle api request

--- system/settings.py
+++ system/settings.py
@@ -235,6 +235,14 @@ REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 10,
     "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"],
+    "DEFAULT_THROTTLE_CLASSES": [
+        "rest_framework.throttling.AnonRateThrottle",
+        "rest_framework.throttling.UserRateThrottle",
+    ],
+    "DEFAULT_THROTTLE_RATES": {
+        "anon": config("ANON_THROTTLE_RATE"),
+        "user": config("USER_THROTTLE_RATE"),
+    },
 }
 
 # CUSTOM USER MODEL
