4801269d5a8e1781fd3d1dc53c2f19449871d595
Kusuma J <sensnerd@gmail.com>
2023-10-28 23:48:36 +0800

    fix: don't recreate temp path

--- tools/fileupload_helper.py
+++ tools/fileupload_helper.py
@@ -1,7 +1,6 @@
 import os
 import cv2
 from django.core.exceptions import ValidationError
-import tempfile
 
 
 def validate_uploaded_file(uploaded_file, type):
@@ -20,26 +19,20 @@ def validate_uploaded_file(uploaded_file, type):
             raise ValidationError("File size exceeds the maximum allowed size of 15 MB")
 
         try:
-            # opencv requires a file path to analyze video files
-            with tempfile.NamedTemporaryFile(delete=False) as temp_file:
-                temp_file.write(uploaded_file.read())
-                video_path = temp_file.name
+            # get the temporary path of the uploaded file
+            video_path = uploaded_file.temporary_file_path()
 
-            # analyze video file
+            # Analyze video file
             video_capture = cv2.VideoCapture(video_path)
             frame_count = int(video_capture.get(cv2.CAP_PROP_FRAME_COUNT))
             fps = int(video_capture.get(cv2.CAP_PROP_FPS))
             video_duration = frame_count / fps
 
-            # release video capture
+            # Release the video capture resource
             video_capture.release()
 
         except Exception as e:
             raise ValidationError("Error analyzing video file: " + str(e))
-
-        finally:
-            # delete temp file
-            os.unlink(video_path)
 
         if video_duration > 35:
             raise ValidationError(
