60adbd61f0683d99e0b8064540806213ed8bc7ed
Kusuma J <sensnerd@gmail.com>
2023-11-18 09:43:57 +0800

    feat: add button verification on admin

--- apps/orders/admin_views.py
+++ apps/orders/admin_views.py
@@ -14,12 +14,19 @@ def confirm_order_view(request, pk):
     if request.user.is_superuser:
         try:
             order = Order.objects.get(pk=pk)
-            send_order_confirmation_email(order.id)
-            order.status = "confirmed"
-            order.save()
-            messages.success(
-                request, f"Order with ID {order.ref_code} is confirmed successfully."
-            )
+            if order.payment_status == "settlement":
+                send_order_confirmation_email(order.id)
+                order.status = "confirmed"
+                order.save()
+                messages.success(
+                    request,
+                    f"Order with ID {order.ref_code} is confirmed successfully.",
+                )
+            else:
+                messages.error(
+                    request,
+                    f"Order with ID {order.ref_code} is not confirmed because payment status is not settlement.",
+                )
         except Exception as e:
             messages.error(request, str(e))
     else:
@@ -32,13 +39,19 @@ def book_shipment_view(request, pk):
     if request.user.is_superuser:
         try:
             order = Order.objects.get(pk=pk)
-            lionparcel_booking(order.id)
-            send_order_shipping_email(order.id)
-            order.status = "shipping"
-            order.save()
-            messages.success(
-                request, f"Order with ID {order.ref_code} is shipped successfully."
-            )
+            if order.status == "confirmed":
+                lionparcel_booking(order.id)
+                send_order_shipping_email(order.id)
+                order.status = "shipping"
+                order.save()
+                messages.success(
+                    request, f"Order with ID {order.ref_code} is shipped successfully."
+                )
+            else:
+                messages.error(
+                    request,
+                    f"Order with ID {order.ref_code} is not shipped because status is not confirmed.",
+                )
         except Exception as e:
             messages.error(request, str(e))
     else:
